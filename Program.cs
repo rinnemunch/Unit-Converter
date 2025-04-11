6c1b8fe1b09175bd02806fb9b50a86084e022508
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-10 21:04:09 -0400

    temp in celsius

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter temperature in Celsius: ");    
+double celsius = Convert.ToDouble(Console.ReadLine());   
+
+double fahrenheit = (celsius * 9 / 5) + 32;
+
+Console.WriteLine($"{celsius}°C is {fahrenheit}°F");  
